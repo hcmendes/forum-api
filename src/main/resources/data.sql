aef14515937e5976ba5c7773eaec3757b2d686cf
Hilton M. Cardoso <hilton.m.cardoso@gmail.com>
2022-09-19 23:56:11 -0300

    proteção com spring security

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$G85bKwZA3R.qIWrYx8Ulb.GVWoGALMBUXYQ1t8JtUzhJ1T/HifQ/C');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
