565f0db8cde70cd24a2f32b677a29f0afa0af752
Hilton M. Cardoso <hilton.m.cardoso@gmail.com>
2024-10-07 17:50:49 -0300

    atualização da configuração do swagger OpenAPI

---
+++ src/main/java/br/com/alura/forum/config/swagger/OpenAPIConfig.java
@@ -0,0 +1,19 @@
+package br.com.alura.forum.config.swagger;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public")
+                .pathsToMatch("/**")
+                .build();
+    }
+
+    // Adicione outras configurações conforme necessário
+}
